8cc040ee61ea15f3f53d9bdd2d51b02039d92035
Miso-sou <work.wadhwani@gmail.com>
2026-04-11 01:03:35 +0530

    changed some stuff

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,9 +3,46 @@ import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 
 const generateToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "10d"})
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "10d" })
 }
 
 const registerUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body
+
+        if (!username || !email || !password) {
+            return res.status(400).json({ message: "All fields are required!" })
+        }
     
+        const [emailExists, usernameExists] = await Promise.all([
+            User.findOne({ email }),
+            User.findOne({ username })
+        ])
+    
+        if (emailExists) {
+            return res.status(409).json({ message: "Email already exists" })
+        }
+    
+        if (usernameExists) {
+            return res.status(409).json({ message: "Username already exists" })
+        }
+    
+        const user = await User.create({
+            username,
+            email,
+            password
+        });
+    
+        res.status(201).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
+    }
 }
